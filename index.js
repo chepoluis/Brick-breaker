9544e3a472f0419e2e17e5389cb929300e5417f9
Jose  luis Villa <vsjoseluisvs@gmail.com>
2021-03-19 07:23:37 -0600

    Ball bounce

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let x = canvas.width / 2;
 let dx = 2;
 let y = canvas.height - 30;
 let dy = -2;
+let ballRadius = 10;
+
+let ballColor = "#0095DD";
 
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
@@ -25,19 +28,43 @@ ctx.closePath();
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = ballColor;
     ctx.fill();
     ctx.closePath();
 }
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas, first two parameter are the positions X and Y
-                                                      // The other second arguments are the space that will be clear, therefore cleans all the canvas
-
+    // Clear the canvas, first two parameter are the positions X and Y
+    // The other second arguments are the space that will be clear, therefore cleans all the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height); 
     drawBall();
+
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+        ballColor = randomColor();
+    }
+
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+        ballColor = randomColor();
+    }
+
     x += dx;
     y += dy;
+
+    //console.log(randomColor());
+}
+
+function randomColor() {
+    let symbols = '0123456789ABCDEF';
+    let color = '#';
+
+    for (let i = 0; i < 6; i++) {
+        color = color + symbols[Math.floor(Math.random() * 16)];    
+    }
+
+    return color;
 }
 
 setInterval(draw, 10);
